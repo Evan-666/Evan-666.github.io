0ec5ba7876ca15d92c1a1c669257dc47ff9beb07
chen <1937244887@qq.com>
2025-12-16 13:18:32 +0800

    Add files via upload

---
+++ Three.js
@@ -0,0 +1,5 @@
+import * as THREE from 'three';
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
+import { RoomEnvironment } from 'three/addons/environments/RoomEnvironment.js'; 
